4882939a078e86d7256478348bf6c89fcefaf523
gcosentino1 <85801475+joeycose@users.noreply.github.com>
2023-02-17 10:33:08 -0500

    Feat # 2 : Updated Navbar

--- my-app/components/MainNav.js
+++ my-app/components/MainNav.js
@@ -1,27 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavDropdown } from 'react-bootstrap';
 import styles from './MainNav.module.css';
-
-
 
 export default function MainNav() {
     return (
-        <Navbar bg="dark" variant="dark">
+        <Navbar className={`${styles.NavMain}`} collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Nav className={`me-auto ${styles.navLinks}`}>
-                    <Nav.Link href="#twitch">Twitch</Nav.Link>
-                    <Nav.Link href="#instagram">Instagram</Nav.Link>
-                    <Nav.Link href="#tiktok">Youtube</Nav.Link>
-                    <Nav.Link href="#tiktok">Discord</Nav.Link>
-                    <Nav.Link href="#tiktok">TikTok</Nav.Link>
-                </Nav>
-                <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-                    </Nav.Link>
-                </Nav>
+                <Navbar.Brand className={` ${styles.centeredLink}`} href="#">Matan Evenoff</Navbar.Brand>
+                <Navbar.Toggle className="ml-auto" aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className={`me-auto ${styles.navLinks}`}>
+                        <Nav.Link href="#twitch">Twitch</Nav.Link>
+                        <Nav.Link href="#instagram">Instagram</Nav.Link>
+                        <Nav.Link href="#youtube">Youtube</Nav.Link>
+                        <Nav.Link href="#discord">Discord</Nav.Link>
+                        <Nav.Link href="#tiktok">TikTok</Nav.Link>
+                    </Nav>
+                    <Nav className={`${styles.navLinksTwo} d-flex align-items-center`}>
+                        <Nav.Link href="#merch">Merch</Nav.Link>
+                        <Nav.Link href="#positions">Positions</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
