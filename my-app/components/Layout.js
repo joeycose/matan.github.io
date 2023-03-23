d2f283d4ea03ef48872f3ac4cff0742983c1a776
gcosentino1 <joeycaze@gmail.com>
2023-03-22 20:01:09 -0400

    fixed footer positioning

--- my-app/components/Layout.js
+++ my-app/components/Layout.js
@@ -4,14 +4,14 @@ import MainSection from "./MainSection";
 
 const Layout = ({ children }) => {
     return (
-        <>
-            <div style={{ backgroundColor: "black" }}>
+        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+            <div style={{ backgroundColor: "black", flex: "1" }}>
                 <MainNav />
                 <MainSection />
-                <div style={{ paddingBottom: "calc(330px + 1rem)" }}>{children}</div>
-                <Footer style={{ position: "fixed", bottom: 0, width: "100%", height: "80px" }} />
+                <div style={{ paddingBottom: "calc(330px + 1rem)", flex: "1" }}>{children}</div>
             </div>
-        </>
+            <Footer style={{ width: "100%", height: "80px" }} />
+        </div>
     );
 };
 
